6d2acda717d06f05accb9a5e6a68153025bb197e
annie <annie@Annie-PC>
2023-08-10 01:54:14 +0530

    Aug 9th CW & HW

--- MavenProject/src/main/java/SDET_Programs/ArmstrongNumber.java
+++ MavenProject/src/main/java/SDET_Programs/ArmstrongNumber.java
@@ -26,23 +26,51 @@ public class ArmstrongNumber {
 		boolean actOutput = findArmstrong(n);
 		Assert.assertEquals(output, actOutput);
 	}
+	
+	@Test
+	public void test2() {
+		int n = 1634;
+		boolean output = true;
+		boolean actOutput = findArmstrong(n);
+		Assert.assertEquals(output, actOutput);
+	}
 
+	@Test
+	public void test3() {
+		int n = 1635;
+		boolean output = false;
+		boolean actOutput = findArmstrong(n);
+		Assert.assertEquals(output, actOutput);
+	}
 
 	public boolean findArmstrong(int n) {
 		
 		int x=n;
+		int res = 0, res1 = 0;
 		String digits = String.valueOf(n);
 		int len = digits.length();
 	
 		int temp = 0, n1=0;
 		while(n>0) {
 			n1 = n%10; //153 %10 = 3 //15%10=5 //1%10 = 1
-			temp = temp + (n1*n1*n1); // 27+15+1
+			res = findPower(n1,len);
+			res1 = res1 + res ; // 27+15+1
 			n = n/10; // 153/10 = 15 //15/10 = 1/10 = 0
 			
 		}
-		if(temp==x) return true;
+		if(res1==x) return true;
 		else return false;
+	}
+	
+	public int findPower(int base,int exponent) {
+		
+		int res = 1;
+		while(exponent!=0) {
+			res = res * base;
+			exponent--;
+		}
+		return res;
+		
 	}
 	
 	
